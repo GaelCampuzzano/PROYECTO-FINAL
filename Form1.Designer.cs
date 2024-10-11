ea7acb0db77b290527b45c1099b1b598af9cd454
Maximo <luismaximonava@gmail.com>
2024-10-11 08:16:49 -0700

    gfg

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 111);
+            this.button1.Location = new System.Drawing.Point(317, 149);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(207, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Menú";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- Tiempos_Platillos.Designer.cs
+++ Tiempos_Platillos.Designer.cs
@@ -239,7 +239,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1137, 486);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label9);
