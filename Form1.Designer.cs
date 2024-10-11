a472b536f9dee19459378de61f85a35d0dee455c
Campuzano <43223041@lcb.unidep.edu.mx>
2024-10-11 07:07:56 -0700

    first commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,20 +33,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 111);
+            this.button1.Location = new System.Drawing.Point(113, 89);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(207, 47);
+            this.button1.Size = new System.Drawing.Size(184, 38);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Menú";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
